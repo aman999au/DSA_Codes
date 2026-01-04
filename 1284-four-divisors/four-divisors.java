ae02d19f43521ec22942c9b6656a6434cb41a6cb
Md Amanullah <aman999.u@gmail.com>
2026-01-04 23:55:54 +0530

    Time: 6 ms (99.75%) | Memory: 46.3 MB (51.01%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,5 +1,43 @@
 class Solution {
+    // Optimal Approch
+  public boolean isPrime(int num){
+        if(num==1)return false;
+        if(num==2 || num==3)return true;
+        if(num%2==0 || num%3==0)return false;
+        for(int i=5;i*i<=num;i+=6){
+            if(num%i==0 || num%(i+2)==0)return false;
+        }
+        return true;
+    }
+    public  int sumFourDivisors(int[] nums) {
+       int total=0;
+		for(int num:nums){
+			// for cube root -> 1+p+p^2+p^3
+			int p = (int)Math.round(Math.cbrt(num));
+			if((long)p*p*p == num && isPrime(p)){
+				total+=(1+p+p*p+num);
+				continue;
+			}
+
+			//  coprime p*q
+			for(int i=2;i*i<=num;i++){
+
+				if(num%i==0){
+					int j = num/i;
+					if(i!=j && isPrime(i) && isPrime(j))
+						total+=1+i+j+num;
+					break;
+				}
+			}
+		}
+		return total;
+	}
+    
+    // Brute Force Approch
+    /* 
     public int sumFourDivisors(int[] nums) {
+
+       
           Map<Integer,HashSet<Integer>>mp = new HashMap<>();
 		int ans = 0;
 
@@ -19,7 +57,6 @@ class Solution {
 						
 					}
 				}
-				// System.out.println(mp);
 			}
 		}
 		
@@ -30,5 +67,9 @@ class Solution {
 			}
 		}
 		return ans;
-    }
+        }
+        */
+
+
+    
 }
