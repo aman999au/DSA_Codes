fe28807084a9e34f310011393a60147799f6e7a9
Md Amanullah <aman999.u@gmail.com>
2026-01-11 00:00:30 +0530

    Time: 8 ms (99.85%) | Memory: 46.3 MB (71.09%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,4 +1,30 @@
 class Solution {
+    // DP
+    public int minimumDeleteSum(String s1, String s2) {
+		char[]x = s1.toCharArray();
+		char[]y = s2.toCharArray();
+		int m = x.length;
+		int n = y.length;
+		int[][] dp = new int[m+1][n+1];
+		int total=0;
+		for(char c:x)total+=(int)c;
+		for(char c:y)total+=(int)c;
+
+		for(int i=m-1;i>=0;i--){
+			for(int j=n-1;j>=0;j--){
+				if(x[i]==y[j]){
+					dp[i][j] += x[i]+dp[i+1][j+1];
+			
+				}else{
+					dp[i][j]= Math.max(dp[i][j+1], dp[i+1][j]);
+				}
+			}
+		}
+		return total-dp[0][0]*2;
+		
+    }
+    /* 
+    // Recursion + Memorization
     int m , n;
     int[][]dp = new int[1001][1001];
 	public int solve(char[] s1,char[] s2,int i,int j){
@@ -22,4 +48,5 @@ class Solution {
         }
 		return solve(s1.toCharArray(), s2.toCharArray(), 0, 0);
     }
+    */
 }
