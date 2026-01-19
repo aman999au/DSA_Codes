6f377a1d8e99888deb5af2c582d8deb50e787d5c
Md Amanullah <aman999.u@gmail.com>
2026-01-20 00:03:03 +0530

    Time: 11 ms (27.41%) | Memory: 53.7 MB (94.42%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maxSideLength(int[][] mat, int threshold) {
-        // Using 2D Prefix Sum and finding best side square - Approch 1
-        // TC:  O(rows * cols * min(rows, cols))
+        
         int rows = mat.length;
 		int cols = mat[0].length;
 		int[][] prefix = new int[rows][cols];
@@ -12,7 +11,10 @@ class Solution {
 			}
 		}
 
-		int best=0;
+        // Using 2D Prefix Sum and finding best side square - Approch 1
+        // TC:  O(rows * cols * min(rows, cols))
+		/*
+        int best=0;
 
 		for(int i=0;i<rows;i++){
 			for(int j=0;j<cols;j++){
@@ -34,5 +36,37 @@ class Solution {
 			}
 		}
 		return best;  
+        */
+
+        // Binary Search Approch on sides
+        int low =0,high=Math.min(cols, rows);
+		int result=0;
+		while(low<=high){
+			int mid = low + (high-low)/2;
+			if(check(mid,rows,cols,threshold,prefix)){
+				result=mid;
+				low=mid+1;
+			}else high=mid-1;
+			
+		}
+		return result;
     }
+    private boolean check(int side, int rows, int cols, int threshold, int[][] prefix) {
+		if(side==0)return true;
+
+		for(int i=0;i+side-1<rows;i++){
+			for(int j=0;j+side-1<cols;j++){
+				int r2=i+side-1;
+				int c2=j+side-1;
+
+				int sum = prefix[r2][c2];
+				if(i>0)sum-=prefix[i-1][c2];
+				if(j>0)sum-=prefix[r2][j-1];
+				if(i>0 && j>0)sum+=prefix[i-1][j-1];
+					if(sum<=threshold)
+						return true;
+			}
+		}
+		return false;
+	}
 }
